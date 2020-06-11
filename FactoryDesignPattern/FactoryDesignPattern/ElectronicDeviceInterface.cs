bb40ce78b3441923dbb35c9fafd40f3005ff98a4
Alexandros Michalaris <alex.mihas29@gmail.com>
2020-06-11 18:31:02 +0300

    Created Operating System. Initialized printer.

---
+++ FactoryDesignPattern/FactoryDesignPattern/ElectronicDeviceInterface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryDesignPattern
+{
+    interface ElectronicDeviceInterface
+    {
+        public Guid setMACaddress();
+    }
+}
---
+++ FactoryDesignPattern/FactoryDesignPattern/OperatingSystem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryDesignPattern
+{
+    class OperatingSystem : ElectronicDeviceInterface, SoftwareInterface
+    {
+        private Guid mac;
+
+        public OperatingSystem()
+        {
+            this.mac = setMACaddress();
+        }
+
+        public void createSoftware()
+        {
+            Console.WriteLine("Creating Software...");
+            createKernel();
+        }
+
+        public void createKernel()
+        {
+            Console.WriteLine("Creating Kernel...");
+        }
+
+        public Guid setMACaddress()
+        {
+            return Guid.NewGuid();
+        }
+    }
+}
